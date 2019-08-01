a62d2e5c86e9f9e73f22f0536f2794f157c41b77
DuanYuFei <duanyufei@dayuwuxian.com>
2019-08-01 13:03:44 +0800

    Add Scaffold

--- lib/widgets/MovieDetailPage.dart
+++ lib/widgets/MovieDetailPage.dart
@@ -14,46 +14,51 @@ class MovieDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Placeholder(
-                fallbackWidth: 90,
-                fallbackHeight: 160,
-              ),
-              Column(
-                children: <Widget>[
-                  Text(
-                    "title",
-                    style: TextStyle(fontSize: 40),
-                  ),
-                  Text(
-                    "popular",
-                    style: TextStyle(fontSize: 30),
-                  ),
-                  Text(
-                    "genres",
-                    style: TextStyle(fontSize: 30),
-                  ),
-                  Text(
-                    "language",
-                    style: TextStyle(fontSize: 30),
-                  ),
-                  Text(
-                    "date",
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Text("movie desc"),
-          Text("people portrait gallery"),
-          Text("status"),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Movie detail"),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Placeholder(
+                  fallbackWidth: 90,
+                  fallbackHeight: 160,
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      "title",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    Text(
+                      "popular",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "genres",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "language",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "date",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Text("movie desc"),
+            Text("people portrait gallery"),
+            Text("status"),
+          ],
+        ),
       ),
     );
   }
